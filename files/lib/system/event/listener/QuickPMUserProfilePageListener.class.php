4c448749ae2cb2f9576d731bc652deb5613e47a6
Tim Düsterhus <timwolla@arcor.de>
2011-09-01 22:13:46 +0200

    Cleanup and comment class

--- files/lib/system/event/listener/QuickPMUserProfilePageListener.class.php
+++ files/lib/system/event/listener/QuickPMUserProfilePageListener.class.php
@@ -1,29 +1,35 @@
 <?php
+// wcf imports
 require_once(WCF_DIR.'lib/system/event/EventListener.class.php');
 require_once(WCF_DIR.'lib/form/MessageForm.class.php');
 
+/**
+ * Includes the Quick-PM Template
+ *
+ * @author Martin Schwendowius
+ * @package de.wbb3addons.wcf.quick-pm
+ */
 class QuickPMUserProfilePageListener implements EventListener {
-
-	public $permissionType = 'message';
+	const PERMISSION_TYPE = 'message';
 	public $parseURL = 0;
 	public $enableSmilies = 0;
 	public $enableBBCodes = 0;
 	public $showSignature = 0;
 
+	/**
+	 * @see EventListener::execute()
+	 */
 	public function execute($eventObj, $className, $eventName){
 		$user = $eventObj->user;
 
 		if (WCF::getUser()->userID) {
-			$this->parseURL = WCF::getUser()->{$this->permissionType.'ParseURL'};
-			$this->enableSmilies = WCF::getUser()->{$this->permissionType.'EnableSmilies'};
-			$this->enableBBCodes = WCF::getUser()->{$this->permissionType.'EnableBBCodes'};
-			$this->showSignature = WCF::getUser()->{$this->permissionType.'ShowSignature'};
+			$this->parseURL = WCF::getUser()->{self::PERMISSION_TYPE.'ParseURL'};
+			$this->enableSmilies = WCF::getUser()->{self::PERMISSION_TYPE.'EnableSmilies'};
+			$this->enableBBCodes = WCF::getUser()->{self::PERMISSION_TYPE.'EnableBBCodes'};
+			$this->showSignature = WCF::getUser()->{self::PERMISSION_TYPE.'ShowSignature'};
 		}
 		else {
-			$this->parseURL = MESSAGE_FORM_DEFAULT_PARSE_URL;
-			$this->enableSmilies = MESSAGE_FORM_DEFAULT_ENABLE_SMILIES;
-			$this->enableBBCodes = MESSAGE_FORM_DEFAULT_ENABLE_BBCODES;
-			$this->showSignature = 0;
+			return;
 		}
 
 		WCF::getTPL()->assign(array(
@@ -34,7 +40,7 @@ class QuickPMUserProfilePageListener implements EventListener {
 			'showSignature' => $this->showSignature
 		));
 
-		WCF::getTPL()->append('additionalContents3', WCF::getTPL()->fetch('quickPn'));
+		WCF::getTPL()->append('additionalContents3', WCF::getTPL()->fetch('quickPM'));
 	}
 }
 ?>
