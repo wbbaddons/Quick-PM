2b4361ba49be612283cda9ffe966d202d6cf9666
sani9000 <support@wbb3addons.de>
2011-09-03 17:07:28 +0200

    Corrected PHP-Documentation

--- files/lib/system/event/listener/QuickPMUserPageListener.class.php
+++ files/lib/system/event/listener/QuickPMUserPageListener.class.php
@@ -7,7 +7,9 @@ require_once(WCF_DIR.'lib/form/MessageForm.class.php');
  * Includes the Quick-PM Template
  *
  * @author Martin Schwendowius
+ * @copyright 2008-2011 wbbaddons.de
  * @package de.wbbaddons.wcf.quick-pm
+ * @license LGPL - Lesser General Public License <http://www.gnu.org/licenses/lgpl.html>
  */
 class QuickPMUserPageListener implements EventListener {
 	const PERMISSION_TYPE = 'message';
@@ -20,10 +22,8 @@ class QuickPMUserPageListener implements EventListener {
 	 * @see EventListener::execute()
 	 */
 	public function execute($eventObj, $className, $eventName){
-		// check permissions
 		if (!WCF::getUser()->userID || !WCF::getUser->getPermission('user.pm.canUseQuickPm') || !WCF::getUser->getPermission('user.pm.canUsePm') || !QUICK_PM_ACTIVE) return;
-		
-		//set form values
+
 		$this->parseURL = WCF::getUser()->{self::PERMISSION_TYPE.'ParseURL'};
 		$this->enableSmilies = WCF::getUser()->{self::PERMISSION_TYPE.'EnableSmilies'};
 		$this->enableBBCodes = WCF::getUser()->{self::PERMISSION_TYPE.'EnableBBCodes'};
@@ -42,4 +42,3 @@ class QuickPMUserPageListener implements EventListener {
 		WCF::getTPL()->append('additionalContent3', WCF::getTPL()->fetch('quickPm'));
 	}
 }
-?>
