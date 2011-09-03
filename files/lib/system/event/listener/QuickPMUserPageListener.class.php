851764600cd990fd9325489a7d09f64449ab0513
sani9000 <support@wbb3addons.de>
2011-09-03 17:14:35 +0200

    Deleted some crap ;)

--- files/lib/system/event/listener/QuickPMUserPageListener.class.php
+++ files/lib/system/event/listener/QuickPMUserPageListener.class.php
@@ -1,7 +1,6 @@
 <?php
 // wcf imports
 require_once(WCF_DIR.'lib/system/event/EventListener.class.php');
-require_once(WCF_DIR.'lib/form/MessageForm.class.php');
 
 /**
  * Includes the Quick-PM Template
